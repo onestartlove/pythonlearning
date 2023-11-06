137f65c8188762348d1d46bb43cd74de958b5e5d
onestartlove <146115580+onestartlove@users.noreply.github.com>
2023-11-06 22:14:03 +0800

    Delete task5.py

--- task5.py
+++
@@ -1,9 +0,0 @@
-students ={
-    "001":"Amy",
-    "002":"Bob",
-    "003":"Cc",
-    "004":"David",
-    "005":"Eve"
-}
-filtered_students={k:v for k,v in students.items()if int(k[-1])%2!=0}
-print(filtered_students)
