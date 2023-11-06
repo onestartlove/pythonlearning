b12faca1f53886f86769ecc621bf7eed7efa286f
onestartlove <146115580+onestartlove@users.noreply.github.com>
2023-11-06 22:14:20 +0800

    Delete task6.py

--- task6.py
+++
@@ -1,8 +0,0 @@
-def count_numbers(numbers):
-    counts = {}
-    for num in numbers:
-        if num in counts:
-            counts[num] += 1
-        else:
-            counts[num] = 1
-    return counts
