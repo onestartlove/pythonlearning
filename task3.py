ca3a19fe95313c5ad8222cff815413652f30fbfb
onestartlove <146115580+onestartlove@users.noreply.github.com>
2023-11-06 22:12:13 +0800

    Delete task3.py

--- task3.py
+++
@@ -1,7 +0,0 @@
-string=input("请输入一个字符串")
-if"ol" in string:
-    new_string=string.replace("ol","fzu")
-    reversed_string=new_string[::-1]
-    print(reversed_string)
-else:
-    print("字符串中不含有ol")
